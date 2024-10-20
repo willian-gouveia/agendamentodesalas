98a9807897ae4d4b33dc7985acccfea5f2fc44d9
Willian Gouveia <w.g08@hotmail.com>
2024-10-20 14:05:35 -0300

    Add files via upload

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,7 @@
-const config = {
-  dbHost: process.env.MYSQL_HOST,
-  dbName: process.env.MYSQL_DATABASE,
-  dbPort: process.env.MYSQL_PORT,
-  dbUser: process.env.MYSQL_USERNAME,
-  dbPassword: process.env.MYSQL_PASSWORD,
+module.exports = {
+  host: 'localhost',
+  port: 3306,
+  database: 'gerenciamentodesalas',
+  user: 'root',
+  password: '',
 }
-
-module.exports = { config };
-
